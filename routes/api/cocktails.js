78b88ddfdbc2c9e6da8a0fd700153c65b4937bd4
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-08-25 01:13:13 +0300

    adds /recipes/:id endpoint ctrl

--- routes/api/cocktails.js
+++ routes/api/cocktails.js
@@ -1,12 +1,19 @@
 import express from "express";
 import {
   getAllCategories,
+  getDrinkById,
   getDrinksByQuery,
   getMainDrinks,
 } from "../../controllers/recipes/index.js";
 
 // import schemas from "../../schemas/cocktailSchema.js";
-// import { validateBody, isEmptyBody, isValidId, authenticate, upload } from "../../middlewares/index.js";
+import {
+  validateBody,
+  isEmptyBody,
+  isValidId,
+  authenticate,
+  upload,
+} from "../../middlewares/index.js";
 
 const router = express.Router();
 
@@ -15,6 +22,8 @@ const router = express.Router();
 router.get("/category-list", getAllCategories);
 
 router.get("/main-page", getMainDrinks);
+
+router.get("/:id", isValidId, getDrinkById);
 
 router.get("/", getDrinksByQuery);
 
