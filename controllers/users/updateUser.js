6480e6460b0875e17728384403c4804de09a4f84
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-08-27 20:45:36 +0300

    updates res.json from updateUser.js

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -2,18 +2,17 @@ import { ctrlWrapper } from "../../utils/index.js";
 import User from "../../models/users.js";
 
 const updateUser = async (req, res) => {
+  const { _id } = req.user;
+  const { name } = req.body;
 
-    const { _id } = req.user;
-    const { name } = req.body;
-    if (req.file) {
-      const avatarURL = req.file.path;
-      await User.findByIdAndUpdate(_id, { avatarURL, name });
-      res.json({ avatarURL, name });
-    }
-    if(!req.file){
-      await User.findByIdAndUpdate(_id, { name });
-      res.json({ name });
-    }
+  if (req.file) {
+    const avatarURL = req.file.path;
+    await User.findByIdAndUpdate(_id, { avatarURL, name });
+  } else {
+    await User.findByIdAndUpdate(_id, { name });
+  }
+
+  res.json({ avatarURL, name });
 };
 
 export default ctrlWrapper(updateUser);
