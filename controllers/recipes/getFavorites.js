f6653a523e82db87eae027e506aa51f714ff6efa
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-08-27 17:08:26 +0300

    updates getFavorites with pagination

--- controllers/recipes/getFavorites.js
+++ controllers/recipes/getFavorites.js
@@ -2,12 +2,24 @@ import { ctrlWrapper } from "../../utils/index.js";
 import Cocktail from "../../models/cocktails.js";
 
 const getFavorites = async (req, res) => {
+  const { page = 1, limit = 8 } = req.query;
   const { _id: user } = req.user;
-  const favorites = await Cocktail.find({ users: user });
+
+  const skip = (page - 1) * limit;
+
+  const favorites = await Cocktail.find({ users: user })
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit)
+    .exec();
+
   if (!favorites || favorites.length === 0) {
     return res.status(404).json({ error: "No favorite drinks" });
   }
-  res.json(favorites);
+
+  const totalHits = await Cocktail.countDocuments({ users: user });
+
+  res.json({ totalHits, favorites });
 };
 
 export default ctrlWrapper(getFavorites);
